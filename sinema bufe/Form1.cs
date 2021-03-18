3d9b1bbdaf91c3a61f8ae837707ee49054e7325b
rumeysayuk90@gmail.com <rumeysayuk90@gmail.com>
2021-03-18 21:55:39 +0300

    .

--- sinema bufe/Form1.cs
+++ sinema bufe/Form1.cs
@@ -38,16 +38,8 @@ namespace sinema_bufe
             sinerapor.Parameters["su"].Value = txtSu.Text;
             sinerapor.Parameters["bilet"].Value = txtBilet.Text;
             sinerapor.Parameters["toplam"].Value = lblSonuc.Text;
-            sinerapor.Parameters["qrCode"].Value = "05068897267";//qr kod okutulunca beni aricak mesela
-            // bu qr koda müşteriye özel unique kod üretip onu basabilirsin
-            //onu hashlersin kırılamazda bu sayede kasadan bu bileti alır
-            // mısırı vs alacağı yere gittiğinde bu kod okutulduğunda işlem yapabilir
-            //şimdilik o unique kod(sipariş id gibi) yerine direk değerleride basabilirsin
-            //mesela 2 mısır 3 çay 0 su aldığnı varsayalarım
-            //002003000 numarasını basabilirsin direk kasada okutulduğunda ilk 3 hane mısır sonra çay sonra su 
-            // diye işlem yapabilirsin teşekkür ederim bu öğrettiklerin için :)  rica eerim :)
-            // varmı sormak istediğin birşey ben bunları bi sindireyim öyle sorarım olursa tamam kolaygeslin :)
-            //teşekkür ederim
+            sinerapor.Parameters["qrCode"].Value = "05068897267";
+
             sinerapor.CreateDocument();
             sinerapor.Print();
             
